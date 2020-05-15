5dab99098041e01b0a37a39ca6457610e15a2c1e
rajasekhar790 <Raja.sekhar790@aol.com>
2020-05-15 07:17:07 +0530

    Create Challenge_1.scala

---
+++ Challenge_1.scala
@@ -0,0 +1,41 @@
+object Solution {
+    def main(args: Array[String]) {
+        val i = 4
+        val d = 4.0
+        val s = "HackerRank "
+
+
+  val x = scala.io.StdIn.readInt()
+
+  val y = scala.io.StdIn.readDouble()
+
+  val z = scala.io.StdIn.readLine()
+      
+
+
+		// Read values of another integer, double, and string variables
+        // Note use scala.io.StdIn to read from stdin
+        
+        // Print the sum of both the integer variables
+        
+         val isum = x + i
+           println(isum)
+
+
+        // Print the sum of both the double variables
+        
+
+         val dsum = y+d
+         println(dsum)
+
+        // Concatenate and print the string variables
+      
+      println(s"$s$z" )
+
+      
+        // The 's' variable above should be printed first.
+
+
+
+    }
+}
